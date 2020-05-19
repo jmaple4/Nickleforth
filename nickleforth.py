cbbede1751f5384e1427448e19ddde578095486d
Nick Kovac <nickkova@amazon.com>
2020-05-19 09:33:03 -0700

    All the files need to run this

---
+++ nickleforth.py
@@ -0,0 +1,21 @@
+# This is a program that prompts for a file name, then opens that file and 
+# reads through the file, and print the contents of the file backwards. 
+# you know...so Nicklebackk lyrics look like devil verses
+
+fileName = input("Enter file name: ")
+
+try:
+    fileHandle = open(fileName)
+except:
+    print('File cannot be opened:', fileName)
+    quit()
+
+print("\nThese are the satanic verses\n".upper())
+
+for line in fileHandle:
+    line = line.rstrip()[::-1]
+    print(line)
+# line = line.rstrip()[::-1] # this slicing reverses the strings characters: [::-1]
+# line = line[::-1] # this slicing reverses the strings characters: [::-1]
+# print('Here it is as satanic verse')
+# print(line)
